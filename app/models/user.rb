fb2434a3a0575bcbf6e4cc6e00bab6757c3a76d7
Rashmi Yadav <rashmi.yadav@mynewsdesk.com>
2014-12-10 13:02:07 +0100

    add signup and user and update create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
+    where(uid: auth.uid).first_or_initialize.tap do |user|
       user.uid = auth.uid
       user.name = auth.info.name
       user.email = auth.info.email
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'sessions#create'
+  delete '/signout' => 'sessions#destroy'
   root "home#index"
 end
