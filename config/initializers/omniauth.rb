b9c878190c1b1f4eb5130349dd3667e9f57857e7
Rashmi Yadav <rashmi.yadav@mynewsdesk.com>
2014-12-14 22:07:24 +0100

    update redirect uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    redirect_uri:'http://lvh.me:3000/auth/google_oauth2/callback'
+    redirect_uri:'http://calendar-report.herokuapp.com/auth/google_oauth2/callback'
   }
 end
